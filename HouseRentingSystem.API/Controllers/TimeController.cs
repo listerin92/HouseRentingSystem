bcd5c1a60775b981c3799cc3234e6ed548f42e20
Dobromir Petrov <luzterin@gmail.com>
2022-11-06 22:15:39 +0200

    cors api

---
+++ HouseRentingSystem.API/Controllers/TimeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("get")]
+        public IActionResult GetCurrentTime()
+        {
+            return Ok(new { currentTime = DateTime.Now.ToLocalTime() });
+        }
+    }
+}
---
+++ HouseRentingSystem.API/Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(
+//    options =>
+//{
+//    options.AddPolicy(name: "MyJSPolicy", policy =>
+//    {
+//        policy.WithOrigins("http://127.0.0.1:5500")
+//            .AllowAnyMethod()
+//            .AllowAnyHeader();
+//    });
+//}
+    );
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors(builder => builder.WithOrigins("http://127.0.0.1:5500"));
+//app.UseCors("MyJSPolicy");
+
+app.MapControllers();
+
+app.Run();
